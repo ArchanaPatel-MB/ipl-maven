14af3113c38746238d904216c81d3b5360bd17c5
Archana Patel <archana.patel.16@mountblue.tech>
2021-08-19 10:41:25 +0530

    Initial commit for ipl maven project

---
+++ src/main/java/com/ipl/Ipl.java
@@ -0,0 +1,255 @@
+package com.ipl;
+import com.ipl.model.Match;
+import com.ipl.model.Delivery;
+
+import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Ipl {
+    public static int matchId = 0;
+    public static int inning = 1;
+    public static int battingTeam = 2;
+    public static int bowlingTeam = 3;
+    public static int over = 4;
+    public static int ball = 5;
+    public static int batsman = 6;
+    public static int non_striker = 7;
+    public static int bowler = 8;
+    public static int is_super_over = 9;
+    public static int wide_runs = 10;
+    public static int bye_runs = 11;
+    public static int legbye_runs = 12;
+    public static int noball_runs = 13;
+    public static int penalty_runs = 14;
+    public static int batsman_runs = 15;
+    public static int extra_runs = 16;
+    public static int total_runs = 17;
+    public static int player_dismissed = 18;
+    public static int dismissal_kind = 19;
+    public static int fielder = 20;
+
+
+    public static int id = 0;
+    public static int season = 1;
+    public static int city = 2;
+    public static int date = 3;
+    public static int team1 = 4;
+    public static int team2 = 5;
+    public static int tossWinner = 6;
+    public static int tossDecision = 7;
+    public static int result = 8;
+    public static int dlApplied = 9;
+    public static int winner = 10;
+    public static int winByRuns = 11;
+    public static int winByWicket = 12;
+    public static int playerOfMatch = 13;
+    public static int venue = 14;
+    public static int umpire1 = 15;
+    public static int umpire2 = 16;
+    public static int umpire3 = 17;
+
+    static String pathDeliveries = "./data/deliveries.csv";
+    static String matchesCsv = "./data/matches.csv";
+
+    public List<Match> getMatchData() throws IOException {
+        List<Match> matches = new ArrayList<>();
+        String line = "";
+
+        int skip = 0;
+        BufferedReader bf = new BufferedReader(new FileReader(matchesCsv));
+        while ((line = bf.readLine()) != null) {
+            Match match = new Match();
+            if (skip == 0) {
+                skip++;
+                continue;
+            }
+            String[] matchData = line.split(",");
+            match.setSeason(matchData[season]);
+            match.setWinner(matchData[winner]);
+            match.setMatchId(matchData[matchId]);
+            matches.add(match);
+        }
+//
+        return matches;
+    }
+
+    public  List<Delivery> getDeliveryData() throws IOException {
+        List<Delivery> deliveries = new ArrayList<>();
+        String line = "";
+
+        int skip = 0;
+        BufferedReader bf = new BufferedReader(new FileReader(pathDeliveries));
+        while ((line = bf.readLine()) != null) {
+            Delivery delivery = new Delivery();
+            if (skip == 0) {
+                skip++;
+                continue;
+            }
+            String[] deliveryData = line.split(",");
+            delivery.setExtraRuns(deliveryData[extra_runs]);
+            delivery.setMatchId(deliveryData[matchId]);
+            delivery.setBowlingTeam(deliveryData[bowlingTeam]);
+            delivery.setBowler(deliveryData[bowler]);
+            delivery.setBall(deliveryData[ball]);
+            delivery.setTotalRuns((deliveryData[total_runs]));
+            deliveries.add(delivery);
+        }
+        return deliveries;
+    }
+
+    public HashMap<String, Integer> matchesPlayedPerYear(List<Match> matches) {
+        if(matches.equals(null))
+        {
+//            throw new NullPointerException();
+            return null;
+        }
+        HashMap<String, Integer> matchCountMap = new HashMap<String, Integer>();
+        System.out.println("matches played per year");
+
+        for (Match matchData : matches) {
+            if (matchCountMap.containsKey(matchData.getSeason())) {
+                matchCountMap.put(matchData.getSeason(), matchCountMap.get(matchData.getSeason()) + 1);
+            } else {
+                matchCountMap.put(matchData.getSeason(), 1);
+            }
+        }
+        return matchCountMap;
+    }
+
+    public Map<String, HashMap<String, Integer>> matchesWonPerTeamPerYear(List<Match> matches) {
+        Map<String, HashMap<String, Integer>> matchesWonPerTeam = new HashMap<String, HashMap<String, Integer>>();
+        System.out.println("matches won per team per year");
+
+        for (Match matchData : matches) {
+
+            if (matchesWonPerTeam.containsKey(matchData.getSeason())) {
+                if (matchesWonPerTeam.get(matchData.getSeason()).containsKey(matchData.getWinner())) {
+                    matchesWonPerTeam.get(matchData.getSeason()).put(matchData.getWinner(), matchesWonPerTeam.get(matchData.getSeason()).get(matchData.getWinner()) + 1);
+
+                } else {
+                    matchesWonPerTeam.get(matchData.getSeason()).put(matchData.getWinner(), 1);
+                }
+            } else {
+                matchesWonPerTeam.put(matchData.getSeason(), new HashMap<String, Integer>());
+                matchesWonPerTeam.get(matchData.getSeason()).put(matchData.getWinner(), 1);
+            }
+        }
+        return matchesWonPerTeam;
+    }
+
+    public  HashMap<String, Integer> extraRunConceded(List<Match> matches, List<Delivery> deliveries) {
+        List<String> id = new ArrayList<String>();
+        HashMap<String, Integer> extraRunsByBowlingTeam = new HashMap<>();
+        for (Match matchData : matches) {
+            if (matchData.getSeason().equals("2016")) {
+                id.add(matchData.getMatchId());
+            }
+        }
+        for (Delivery delivery : deliveries) {
+            if (id.contains(delivery.getMatchId())) {
+                if (extraRunsByBowlingTeam.containsKey(delivery.getBowlingTeam())) {
+                    extraRunsByBowlingTeam.put(delivery.getBowlingTeam(), extraRunsByBowlingTeam.get(delivery.getBowlingTeam()) + Integer.parseInt(delivery.getExtraRuns()));
+                } else {
+                    extraRunsByBowlingTeam.put(delivery.getBowlingTeam(), Integer.parseInt(delivery.getExtraRuns()));
+                }
+            }
+        }
+        return extraRunsByBowlingTeam;
+
+    }
+
+    public static HashMap<String, Float> sortByValue(HashMap<String, Float> hm) {
+        List<Map.Entry<String, Float>> list =
+                new LinkedList<Map.Entry<String, Float>>(hm.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<String, Float>>() {
+            public int compare(Map.Entry<String, Float> o1,
+                               Map.Entry<String, Float> o2) {
+                return (o2.getValue()).compareTo(o1.getValue());
+            }
+        });
+
+        HashMap<String, Float> temp = new LinkedHashMap<String, Float>();
+        for (Map.Entry<String, Float> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
+    }
+
+
+    public Map<String, Float> topTenEconomicalBowler(List<Match> matches, List<Delivery> deliveries) {
+        List<String> id = new ArrayList<String>();
+        HashMap<String, Float> topEcomnomicalBowler = new HashMap<>();
+        ArrayList<Float> EcoRate = new ArrayList<Float>();
+
+        HashMap<String, Integer> totalRuns = new HashMap<String, Integer>();
+        HashMap<String, Integer> totalBalls = new HashMap<String, Integer>();
+        for (Match matchData : matches) {
+            if (matchData.getSeason().equals("2015")) {
+                id.add(matchData.getMatchId());
+            }
+        }
+        for (Delivery delivery : deliveries) {
+            if (id.contains(delivery.getMatchId())) {
+                if (totalRuns.containsKey(delivery.getBowler())) {
+                    totalBalls.put(delivery.getBowler(), totalRuns.get(delivery.getBowler()) + Integer.parseInt(delivery.getTotalRuns()));
+                    totalRuns.put(delivery.getBowler(), totalBalls.get(delivery.getBowler()) + Integer.parseInt(delivery.getTotalRuns()));
+                } else {
+                    totalRuns.put(delivery.getBowler(), Integer.parseInt(delivery.getTotalRuns()));
+                    totalBalls.put(delivery.getBowler(), Integer.parseInt(delivery.getBall()));
+                }
+            }
+        }
+        for (String key : totalRuns.keySet())
+        {
+            topEcomnomicalBowler.put(key, totalRuns.get(key).floatValue() / (Float) totalBalls.get(key).floatValue() / 6);
+            EcoRate.add(totalRuns.get(key).floatValue() / (Float) totalBalls.get(key).floatValue() / 6);
+        }
+//        System.out.println(topEcomnomicalBowler );
+//
+//        List<Map.Entry<String, Float>> topTen = new ArrayList<>(topEcomnomicalBowler.entrySet());
+//        System.out.println(topTen);
+//        Collections.sort(topTen, new Comparator<Map.Entry<String, Float>>() {
+//            public int compare(Map.Entry<String, Float> c2,Map.Entry<String, Float> c1)
+//            {
+//
+//                return c1.getValue().compareTo(c2.getValue());
+//            }
+//        });
+
+
+
+        HashMap<String, Float> topTen = sortByValue(topEcomnomicalBowler);
+//        System.out.println(topTen);
+        Map<String, Float> finalBowlers = new HashMap<>();
+        Set<Map.Entry<String, Float>> x = topTen.entrySet();
+        int i = 0;
+        for (Map.Entry<String, Float> y : x) {
+            if (i < 10) {
+                finalBowlers.put(y.getKey(), y.getValue());
+                i++;
+            } else {
+                break;
+            }
+        }
+        return finalBowlers;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Ipl ipl = new Ipl();
+        List<Match> matchData = ipl.getMatchData();
+        List<Delivery> deliveryData = ipl.getDeliveryData();
+//        System.out.println(ipl.matchesPlayedPerYear(matchData));
+//        System.out.println(ipl.matchesWonPerTeamPerYear(matchData));
+//        System.out.println(ipl.extraRunConceded(matchData, deliveryData));
+        System.out.println(ipl.topTenEconomicalBowler(matchData, deliveryData));
+    }
+}
---
+++ src/test/java/com/ipl/test/Test.java
@@ -0,0 +1,184 @@
+package com.ipl.test;
+import com.ipl.Ipl;
+import com.ipl.model.Delivery;
+import com.ipl.model.Match;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    public void testGetMatchData() throws IOException {
+        Ipl ipl = new Ipl();
+        assertNotNull(ipl.getMatchData());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testGetDeliveriesData() throws IOException {
+        Ipl ipl = new Ipl();
+        assertNotNull(ipl.getDeliveryData());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMatchesPlayedNotNull() throws IOException {
+        Ipl ipl = new Ipl();
+        List<Match> helper=new ArrayList<>();
+        List<Match> matches = ipl.getMatchData();
+        for(int i=0;i<10;i++){
+            helper.add(matches.get(i));
+        }
+        assertNotNull(ipl.matchesPlayedPerYear(helper));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMatchesPlayedPerYear() throws IOException {
+        Ipl ipl = new Ipl();
+        List<Match> helper=new ArrayList<>();
+        List<Match> matches = ipl.getMatchData();
+        for(int i=0;i<10;i++){
+            helper.add(matches.get(i));
+        }
+        HashMap<String, Integer> expected = new HashMap<String, Integer>();
+        expected.put("2017", 10);
+        assertEquals(expected,ipl.matchesPlayedPerYear(helper));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testForNullParam() throws IOException {
+        Ipl ipl = new Ipl();
+        // List<Match> m=null;
+        List<Match> matches = ipl.getMatchData();
+        assertThrows(Exception.class, () -> ipl.matchesPlayedPerYear(null));
+//        assertEquals(matches, exception.getMessage());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testForMatchesWon() throws IOException {
+        Ipl ipl = new Ipl();
+        List<Match> helper=new ArrayList<>();
+        List<Match> matches = ipl.getMatchData();
+        for(int i=0;i<10;i++){
+            helper.add(matches.get(i));
+        }
+        Map<String, HashMap<String, Integer>> expected = new HashMap<String, HashMap<String, Integer>>();
+        expected.put("2017", new HashMap<String, Integer>());
+        expected.get("2017").put("Mumbai Indians",2);
+        expected.get("2017").put("Rising Pune Supergiant",1);
+        expected.get("2017").put("Sunrisers Hyderabad",2);
+        expected.get("2017").put("Kings XI Punjab",2);
+        expected.get("2017").put("Delhi Daredevils",1);
+        expected.get("2017").put("Kolkata Knight Riders",1);
+        expected.get("2017").put("Royal Challengers Bangalore",1);
+        assertEquals(expected,ipl.matchesWonPerTeamPerYear(helper));
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testFOrMatchesWonReturningNullValue() throws IOException {
+        Ipl ipl = new Ipl();
+        List<Match> helper=new ArrayList<>();
+        List<Match> matches = ipl.getMatchData();
+        for(int i=0;i<10;i++){
+            helper.add(matches.get(i));
+        }
+        assertNotNull(ipl.matchesWonPerTeamPerYear(helper));
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public void extraRunConceded() throws IOException {
+        Ipl ipl = new Ipl();
+        List<Match> matchesHelper=new ArrayList<>();
+        List<Delivery> deliveryHelper=new ArrayList<>();
+        List<Match> matches = ipl.getMatchData();
+        List<Delivery> delivery = ipl.getDeliveryData();
+        System.out.println(matches.size());
+        for(int i=577;i<600;i++){
+            matchesHelper.add(matches.get(i));
+        }
+        for(int i=136366;i<136700;i++){
+            deliveryHelper.add(delivery.get(i));
+        }
+
+        HashMap<String, Integer> expected=new HashMap<>();
+        expected.put("Delhi Daredevils",1);
+        expected.put("Kolkata Knight Riders",12);
+        System.out.println(ipl.extraRunConceded(matchesHelper,deliveryHelper));
+        assertEquals(expected,ipl.extraRunConceded(matchesHelper,deliveryHelper));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testFOrExtraRunConcededReturningNullValue() throws IOException {
+        Ipl ipl = new Ipl();
+        List<Match> helper=new ArrayList<>();
+        List<Delivery> delivery = ipl.getDeliveryData();
+        List<Match> matches = ipl.getMatchData();
+        for(int i=0;i<10;i++){
+            helper.add(matches.get(i));
+        }
+        assertNotNull(ipl.extraRunConceded(matches,delivery));
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public void testTopTenEconomicalBowler() throws IOException {
+        Ipl ipl = new Ipl();
+        List<Match> matchesHelper=new ArrayList<>();
+        List<Delivery> deliveryHelper=new ArrayList<>();
+        List<Match> matches = ipl.getMatchData();
+        List<Delivery> delivery = ipl.getDeliveryData();
+        System.out.println(matches.size());
+        for(int i=518;i<560;i++){
+            matchesHelper.add(matches.get(i));
+        }
+        for(int i=122714;i<126700;i++){
+            deliveryHelper.add(delivery.get(i));
+        }
+        Map<String, Float> expected=new HashMap<>();
+        expected.put("KA Pollard",(float)(0.18229167));
+        expected.put("GJ Maxwell",(float)0.17592593);
+        expected.put("PP Chawla",(float)0.17479675);
+        expected.put("DJG Sammy",(float)0.18085106);
+        expected.put("DJ Muthuswami",(float)0.17179488);
+        expected.put("JD Unadkat",(float)0.1851852);
+        expected.put("Iqbal Abdulla",(float)0.18253969);
+        expected.put("DJ Hooda",(float)0.17278288);
+        expected.put("RG More",(float)0.17741936);
+        expected.put("UT Yadav",(float)0.17391305);
+
+        assertEquals(expected,ipl.topTenEconomicalBowler(matchesHelper,deliveryHelper));
+
+        System.out.println(ipl.topTenEconomicalBowler(matchesHelper,deliveryHelper));
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testFOrTopTenEcoBowlersReturningNullValue() throws IOException {
+        Ipl ipl = new Ipl();
+        List<Match> helper=new ArrayList<>();
+        List<Delivery> delivery = ipl.getDeliveryData();
+        List<Match> matches = ipl.getMatchData();
+        for(int i=0;i<10;i++){
+            helper.add(matches.get(i));
+        }
+        assertNotNull(ipl.topTenEconomicalBowler(matches,delivery));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testForMatchesSize() throws IOException {
+        Ipl ipl = new Ipl();
+        List<Match> matches = ipl.getMatchData();
+        assertEquals(matches.size(),636);
+
+
+    }
+    @org.junit.jupiter.api.Test
+    public void testForDeliveriesSize() throws IOException {
+        Ipl ipl = new Ipl();
+        List<Delivery> delivery = ipl.getDeliveryData();
+        assertEquals(delivery.size(),150460);
+    }
+}
